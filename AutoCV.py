aa61201e1f306d23d39a5de203b89760cf9a7c81
SaulHR <SaulHR24@hotmail.com>
2024-10-10 22:25:44 -0600

    Se añadio variables para la implementacion de informacion y funcion MensajeBienvenida

---
+++ AutoCV.py
@@ -0,0 +1,14 @@
+Auto_CV= "Programa esperado para la utilizacion de actualizacion de CV"
+Nombre_Creador = "Realizado por Pre.Ing Saul Eduardo Hernadnez Rodriguez"
+Fecha_UltimaActualizacion = "10/10/2024"
+Licencia= "MIT"
+PasosProg = "Instalacion Python"
+
+def MensajeBienvenida():
+    print(Auto_CV) 
+    print (Nombre_Creador) 
+    print (Fecha_UltimaActualizacion)
+    print (Licencia) 
+    print (PasosProg)
+
+MensajeBienvenida()
